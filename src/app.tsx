4c97267ea89b1961566d1664361dd2648877ce3a
MackeyK24 <MackeyK24@gmail.com>
2026-05-19 13:03:56 -1000

    Babylon Toolkit Repo Update

--- src/app.tsx
+++ src/app.tsx
@@ -82,7 +82,7 @@ function Home() {
           <p>Your questions, answered</p>
           <ul>
             <li>
-              <a href="https://github.com/babylontoolkit/agent/blob/main/references/react-framework.md" target="_blank">
+              <a href="https://babylontoolkit.github.io/agent/references/react-framework.md" target="_blank">
                 <img className="logo" src={babylonLogo} alt="" />
                 Babylon Toolkit
               </a>
