65f98ed33becd90ab822b45f8ab9f9b93e887f52
MackeyK24 <MackeyK24@gmail.com>
2026-05-26 09:22:00 -1000

    Adapter Route Updates

--- src/routing/router.tsx
+++ src/routing/router.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import BabylonSceneViewer from '../babylon/system/babylon';
-import ApplicationRoute from '../babylon/system/routing';
 import { ReactRouterNavAdapter } from "./adpter";
 
 export default function PlayRoute() {
